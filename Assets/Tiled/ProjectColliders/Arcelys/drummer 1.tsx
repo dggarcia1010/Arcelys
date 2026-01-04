7ebefe44e8510af90404737e529dee5e9de26168
dggarcia1010 <d.ggarcia1199@gmail.com>
2026-01-04 18:06:43 +0100

    config

---
+++ Assets/Tiled/ProjectColliders/Arcelys/drummer 1.tsx
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="drummer" tilewidth="32" tileheight="32" tilecount="1092" columns="26">
+ <image source="textures/lpc-tavern/preview/drummer.png" width="832" height="1344"/>
+ <tile id="728">
+  <animation>
+   <frame tileid="728" duration="150"/>
+   <frame tileid="730" duration="150"/>
+   <frame tileid="732" duration="150"/>
+   <frame tileid="734" duration="150"/>
+   <frame tileid="736" duration="150"/>
+   <frame tileid="738" duration="150"/>
+   <frame tileid="728" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="729">
+  <animation>
+   <frame tileid="729" duration="150"/>
+   <frame tileid="731" duration="150"/>
+   <frame tileid="733" duration="150"/>
+   <frame tileid="735" duration="150"/>
+   <frame tileid="737" duration="150"/>
+   <frame tileid="739" duration="150"/>
+   <frame tileid="729" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="754">
+  <animation>
+   <frame tileid="754" duration="150"/>
+   <frame tileid="756" duration="150"/>
+   <frame tileid="758" duration="150"/>
+   <frame tileid="760" duration="150"/>
+   <frame tileid="762" duration="150"/>
+   <frame tileid="764" duration="150"/>
+   <frame tileid="754" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="755">
+  <animation>
+   <frame tileid="755" duration="150"/>
+   <frame tileid="757" duration="150"/>
+   <frame tileid="759" duration="150"/>
+   <frame tileid="761" duration="150"/>
+   <frame tileid="763" duration="150"/>
+   <frame tileid="765" duration="150"/>
+   <frame tileid="755" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Assets/Tiled/ProjectColliders/Arcelys/tavern-deco 1.tsx
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tavern-deco" tilewidth="32" tileheight="32" tilecount="896" columns="16">
+ <image source="textures/lpc-tavern/tavern-deco.png" width="512" height="1792"/>
+ <tile id="146">
+  <animation>
+   <frame tileid="146" duration="100"/>
+   <frame tileid="148" duration="100"/>
+   <frame tileid="150" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="147">
+  <animation>
+   <frame tileid="147" duration="100"/>
+   <frame tileid="149" duration="100"/>
+   <frame tileid="151" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="162">
+  <animation>
+   <frame tileid="162" duration="100"/>
+   <frame tileid="164" duration="100"/>
+   <frame tileid="166" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="163">
+  <animation>
+   <frame tileid="163" duration="100"/>
+   <frame tileid="165" duration="100"/>
+   <frame tileid="167" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="177">
+  <animation>
+   <frame tileid="177" duration="100"/>
+   <frame tileid="178" duration="100"/>
+   <frame tileid="179" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="181">
+  <animation>
+   <frame tileid="181" duration="100"/>
+   <frame tileid="182" duration="100"/>
+   <frame tileid="183" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="189">
+  <animation>
+   <frame tileid="189" duration="100"/>
+   <frame tileid="190" duration="100"/>
+   <frame tileid="191" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="205">
+  <animation>
+   <frame tileid="205" duration="100"/>
+   <frame tileid="206" duration="100"/>
+   <frame tileid="207" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="221">
+  <animation>
+   <frame tileid="221" duration="100"/>
+   <frame tileid="222" duration="100"/>
+   <frame tileid="223" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="237">
+  <animation>
+   <frame tileid="237" duration="100"/>
+   <frame tileid="238" duration="100"/>
+   <frame tileid="239" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
